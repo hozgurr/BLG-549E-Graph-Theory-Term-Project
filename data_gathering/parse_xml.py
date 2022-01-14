cac2c6c1beb84e688cdb00bcedef5ce3d44d9bd4
halil <halillozgurr@gmail.com>
2022-01-14 12:31:01 +0300

    fixed output format

--- data_gathering/parse_xml.py
+++ data_gathering/parse_xml.py
@@ -2,4 +2,4 @@
 import pandas as pd
 
 FILE_NAME = "guzergah_all"
-pd.read_xml(f"xml_files/{FILE_NAME}.xml").to_csv("csv_files/{FILE_NAME}.csv")
+pd.read_xml(f"xml_files/{FILE_NAME}.xml").to_csv(f"csv_files/{FILE_NAME}.csv")
